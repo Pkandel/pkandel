8b3bf56514c6c08bbf8522554bc98834c429071d
prakash <unique_prakash2002@yahoo.com>
2017-05-06 01:03:05 +1000

    added background image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,18 +8,32 @@ class About extends Component {
       <div className="container" id="about">
         <div className="profile">
           <div className="profilePicture">
-            <img className="img-circle" src="./images/me.jpeg" alt="Profile" style={{ width: '180px', height: '180px' }} />
+            <img className="img-circle" src="./images/me.jpeg" alt="Profile" />
           </div>
           <b style={{ fontSize: '20px' }}> Prakash Kandel </b> <br />
           Web Developer <br />
           Outcome.Life, Federation University Australia <br />
           Melbourne Australia
             <hr />
-          <div style={{ textAlign: 'left', paddingLeft: '10px', paddingRight: '10px' }}> I am always interested in things that change the world or will change the world.
+          <div className="profileCaption"> I am always interested in things that change the world or will change the world.
             Always ready to accept challenges which I think is the best way to learn new things.
             Never hesitate to say no for things which I don't know and never take my step back
             when I get a chance to learn new technologies
             </div>
+        </div>
+        <div className="aboutMe">
+
+          Hi, Thank you very much for visiting my site. You might already guess that I am a tech guy originally from nepal.
+        I have completed my Bachelors degree in Science major in physics from back home in 2012.<br /><br />
+          I came to australia in 2013 and joined Federation
+        University (Formely University of Ballarat) to study bachelors in IT major Software Engineering. I have completed my degree in 2016. <br /><br />
+
+          Currently, I am working in GADA technology as a Javascript Developer. I am having a great time in the company as I am working
+        with some of the top tech guy. <br /><br />
+
+          Please connect with me in facebook, linkedIn or drop me an email.<br /><br />
+          Thank you again!!!
+
         </div>
       </div>
     );
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,6 +11,7 @@ class Nav extends Component {
     super();
     this.state = ({
       DisplaysmallNavLink: false,
+      selectedNav: '',
     });
     this._renderNav = this._renderNav.bind(this);
     this._hideSmallNav = this._hideSmallNav.bind(this);
@@ -58,30 +59,31 @@ class Nav extends Component {
 
         <div className="smallNav" id="smallNav">
           <div className="smallNavHead" id="smallNavHead">
-
-            <div id="toggle" onClick={this._renderNav}>
-              <div id="first" />
-              <div id="second" />
-              <div id="third" />
-            </div>
             <li>
               <a href="https://www.facebook.com/prakash.kandel.10" target="_blank"> <img src="./images/fb.jpeg" className="img-circle" style={{ width: '20px', heignt: '20px' }} alt="" /></a>
               <a href="mailto:unique.prakash2002@gmail.com?Subject=Hello%20Prakash"> <img src="./images/gmail.png" className="img-circle" style={{ width: '20px', heignt: '20px' }} alt="" /></a>
               <a href="https://www.linkedin.com/in/prakash-kandel-112b47a3/" target="_blank"> <img src="./images/linkedin.jpeg" className="img-circle" style={{ width: '20px' }} alt="" /></a>
             </li>
+            {this.state.selectedNav}
+            <div id="toggle" onClick={this._renderNav}>
+              <div id="first" />
+              <div id="second" />
+              <div id="third" />
+            </div>
+
 
           </div>
           <div className="smallNavLink" id="smallNavLink">
             <ul >
-              <li><NavLink activeClassName="active" exact to="/contact" ><div onClick={this._hideSmallNav}>Contact</div></NavLink></li>
-              <hr />
-              <li><NavLink activeClassName="active" exact to="/projects"><div onClick={this._hideSmallNav}>Project</div></NavLink></li>
-              <hr />
-              <li><NavLink activeClassName="active" exact to="/skills"><div onClick={this._hideSmallNav}>Skill</div></NavLink></li>
+              <li><NavLink activeClassName="active" exact to="/" ><div onClick={this._hideSmallNav}>Home</div></NavLink></li>
               <hr />
               <li><NavLink activeClassName="active" exact to="/about" ><div onClick={this._hideSmallNav}>About</div></NavLink></li>
               <hr />
-              <li><NavLink activeClassName="active" exact to="/" ><div onClick={this._hideSmallNav}>Home</div></NavLink></li>
+              <li><NavLink activeClassName="active" exact to="/skills"><div onClick={this._hideSmallNav}>Skill</div></NavLink></li>
+              <hr />
+              <li><NavLink activeClassName="active" exact to="/projects"><div onClick={this._hideSmallNav}>Project</div></NavLink></li>
+              <hr />
+              <li><NavLink activeClassName="active" exact to="/contact" ><div onClick={this._hideSmallNav}>Contact</div></NavLink></li>
 
             </ul>
           </div>
