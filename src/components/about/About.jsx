b8be0044659ad1188448916de13641b5c5774b68
prakash <unique_prakash2002@yahoo.com>
2017-05-17 01:53:54 +1000

    finish editing about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -27,44 +27,58 @@ class About extends Component {
               <h4> Experince </h4>
               <hr />
               <div className="experience-each">
-                <img alt="projects" src="./images/back-end.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
-                <h3>Back-End</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris  sed odio dui. </p>
-                <p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>
+                <img alt="projects" src="./images/outcome.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
+                <h3>Front-End React Developer</h3>
+                <h4> Outcome.Life</h4>
+                <h5> Mar 2017 - present . 3 Months</h5>
+                <p>I am working as a front end react developer as well as back end using Nodejs, Mongodb, ExpressJs, Keycloak, Docker, Swagger and more... </p>
+                {/*<p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>*/}
               </div>
+              <hr />
               <div className="experience-each">
-                <img alt="projects" src="./images/back-end.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
-                <h3>Back-End</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris  sed odio dui. </p>
-                <p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>
+                <img alt="projects" src="./images/wpa.jpeg" className="img-circle" style={{ width: '71px', height: '71px' }} />
+                <h3>Full Stack Developer</h3>
+                <h4> Wealth Partner Australia (WPA)</h4>
+                <h5> Jul 2015 - Nov 2015 . 5 Months</h5>
+                <p>Worked as a full stack developer for creating a Loan Calculator web appication useing ASP.NET MVC5. </p>
+                {/*<p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>*/}
               </div>
+              <hr />
               <div className="experience-each">
-                <img alt="projects" src="./images/back-end.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
-                <h3>Back-End</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris  sed odio dui. </p>
-                <p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>
+                <img alt="projects" src="./images/atmc.jpeg" className="img-circle" style={{ width: '71px', height: '71px' }} />
+                <h3>Mentor</h3>
+                <h4> Australian Technical and Management College (ATMC)</h4>
+                <h5> Jul 2015 - Nov 2015 . 5 Months</h5>
+                <p>Worked as a mentor for mentoring the students of the subject group of WWW Tech2 (Php and xml) and Information and Security subjects.</p>
               </div>
             </div>
-
+          <hr />
             <div className="education" >
               <h4> Education </h4>
               <hr />
               <div className="education-each">
-                <img alt="projects" src="./images/back-end.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
-                <h3>Back-End</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris  sed odio dui. </p>
+                <img alt="projects" src="./images/atmc.jpeg" className="img-circle" style={{ width: '71px', height: '71px' }} />
+                <h3>Professional Year</h3>
+                <h4> Australian Technical and Management College</h4>
+                <h5> Jul 2016 - Feb 2017</h5>
+                <p>This course helped me a lot to get a real world experience in IT which I could hardly get from the University. </p>
+                {/*<p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>*/}
+              </div>
+              <hr />
+              <div className="education-each">
+                <img alt="projects" src="./images/federation.png" className="img-circle" style={{ width: '71px', height: '71px' }} />
+                <h3>Bachelor Of IT </h3>
+                <h4> Federation University <span style={{marginLeft:"3em"}}> 6.33 GPA </span></h4>
+                <h5>  2013 - 2016</h5>
+                <p>Bachelor Of IT major in Software Engineering </p>
                 <p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>
               </div>
               <div className="education-each">
-                <img alt="projects" src="./images/back-end.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
-                <h3>Back-End</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris  sed odio dui. </p>
-                <p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>
-              </div>
-              <div className="education-each">
-                <img alt="projects" src="./images/back-end.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
-                <h3>Back-End</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris  sed odio dui. </p>
+                <img alt="projects" src="./images/birendra.jpg" className="img-circle" style={{ width: '71px', height: '71px' }} />
+                <h3>Bachelor Of Science</h3>
+                <h4> Tribhuvan University <span style={{ marginLeft: "3em" }}> 70% </span></h4>
+                <h5>  2007 - 2011</h5>
+                <p>Bachelor Of Science major in Physics </p>
                 <p><a className="btn btn-default" href="#" role="button">Learn More &raquo,</a></p>
               </div>
 
